4dd0b444e0b88a06d0f2b0bf3530970a5623fe45
Tayfun <68816657+Programmer-7777@users.noreply.github.com>
2021-10-18 19:30:31 +0500

    Add files via upload

---
+++ collective nouns.py
@@ -0,0 +1,243 @@
+army
+bevy
+band
+a band of robbers
+a board of directors
+a body of downloading
+a bunch of crooks
+a caravan of gypsies
+a choir of singers
+a class of pupils
+a class of students
+a company of actors
+a company of soldiers, and
+a congregation of worshipers
+a crew of sailors   
+a crowd of Spectators
+a crowd of people
+a dynasty of kings
+a galaxy of beautiful women
+a galaxy of movie stars
+a gang of crooks
+a gang of laborers
+a gang of prisoners,
+a gang of robbers
+a gang of thieves
+a group of dancers
+a group of singers
+a horde of savages
+a host of angels
+a line of kings / rulers  
+a mob of rioters
+a pack of Rascals
+a pack of thieves
+a party of friends
+a patrol of policemen
+a posse of policemen
+a regiment of soldiers,
+a staff of employees
+a staff of Servants
+a staff of teachers
+a team of players
+a tribe of natives
+a troop of scouts
+a group of artists
+a group of performers
+a party of friends
+an album of autographs and
+an album of photographs
+an album of stamps
+an anthology of poems
+an archipelago of islands
+a bale of cotton
+a bale of wool
+a basket of fruit
+a batch of bread
+a battery of guns
+a block of flats
+a book of exercises
+a book of notes
+a bouquet of flowers
+a bowl of rice
+a bunch of bananas
+a bunch of flowers
+a bunch of grapes
+a bunch of keys
+a bundle of rags / old clothes
+a bundle of firewood / sticks
+a bundle of hay
+a catalog of prices / goods
+a chain of events
+a chain of mountains
+a chest of drawers
+a cloud of dust
+a Clump of bushes
+a Clump of trees
+a cluster of coconuts
+a cluster of diamonds
+is a cluster of grapes
+a cluster of stars
+a clutch of eggs
+a collection of coins
+a collection of curiosities of
+a collection of pictures
+a collection of relics
+a collection of stamps    
+a column of smoke
+a comb of bananas
+a compendium of games
+a constellation of stars
+is a cluster of diamonds
+is a cluster of stars
+a clutch of eggs
+a crate of fruit
+a crop of apples
+a fall of rain
+a fall of snow
+a fleet of motor-cars / taxis
+a fleet of ships
+a flight of airplanes
+a flight of steps
+a forest of trees
+a galaxy of stars
+a garland of flowers
+a glossary of difficult, words / phrases
+a group of islands
+a grove of trees
+a hail of bullets
+a hand of bananas ( each a finger)
+a harvest of wheat / corn
+a heap of rubbish
+a heap of ruins
+a hedge of bushes
+a heap of stones
+a layer of soil / dirt
+a library of books
+a line of cars
+a list of names
+a mass of ruins / hair
+a necklace of pearls
+an outfit of clothes
+an orchard of fruit trees
+a pack of cards
+a pack of lies
+a packet of cigarettes    
+a packet of letters
+a pair of shoes
+a pencil of rays and
+a quiver of arrows
+a range of hills
+a range of mountains
+a ream of paper
+a reel of thread / film
+a roll of film / cloth
+a rope of pearls
+a row of houses
+a series of events
+a set of china
+a set of clubs,
+a set of tools
+a Sheaf of arrows
+a Sheaf of corn
+a Sheaf of grain
+a Sheaf of papers
+a Sheaf of wheat
+a shower of blows
+a shower of rain
+a stack of arms
+a stack of corn
+a stack of hay
+a stack of timber
+a stack of wood
+a stock of goods
+a stock of wood
+a string of beads
+a string of pearls
+a suit of clothes
+a suite of furniture
+a suite of rooms
+a tuft of grass
+a tuft of hair
+a wad of currency / notes
+a wreath of flowers
+an army of ants
+a bevy of quail
+a brood/flock of chicks /chickens
+a catch of fish
+a cloud of flies
+a cloud of insects
+a cloud of Locusts
+a colony of gulls
+a drove of cattle
+a drove of horses
+a flight of birds
+a flight of doves
+a flight of Locusts
+a flight of swallows
+a flock of birds
+a flock of geese
+a flock of sheep
+a flock of chickens
+a gaggle of geese
+a haul of fish
+a herd of buffaloes
+a herd of cattle
+a herd of deer      
+a herd of elephants
+a herd of goats
+a herd of swine
+a hive of bees
+a host of sparrows
+a kindle of kittens
+a litter of cubs
+a litter of piglets
+a litter of kittens
+a litter of puppies
+a menagerie of wild animals
+a muster of peacocks
+a nest of ants
+a nest of mice
+a nest of rabbits
+a pack of hounds
+a pack of wolves
+a plague of insects
+a plague of Locusts
+a pride of lions    
+a school of Herrings / other small fish
+a school of porpoises
+a school of whales
+a shoal of fish
+a skein of wild geese in flight
+a string of horses
+a stud of horses
+a swarm of ants
+a swarm of bees
+A swarm of insects
+a swarm of locusts
+a team of oxen
+a train of camels
+a tribe of goats
+a troop of lions
+a troop of monkeys
+a zoo of wild animals
+corporation’s army
+audience
+board
+cabinet
+class
+committee
+company
+council
+department
+faculty
+family
+firm jury majority
+group
+minority
+navy
+public
+school
+senate
+society
+team
+troupe
